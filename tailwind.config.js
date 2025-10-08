35d8bd37acf985a2228ec1e19e5519a79194f45a
Shashwat Raj <rajayshashwat@gmail.com>
2025-10-07 17:20:31 -0700

    woofcare intro

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    content: ["./src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}"],
+    theme: {
+        extend: {
+            colors: {
+                wcBg: "#EEB784",
+                wcSecondary: "#FEE7CB",
+                wcBold: "#A66E38",
+                wcButton: "#805832",
+                wcOffwhite: "#F7FFF7",
+                wcText: "#3F2917",
+                wcSecondaryText: "#CAB096",
+                wcTextbox: "#E5D4C4",
+                wcAlert: "#8B131E",
+                wcLink: "#007AFF",
+            }
+        },
+    },
+    plugins: [],
+  };
+  
